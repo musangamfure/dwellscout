0dcc85d5950d96b7be694ac84a32697b45501dbc
Musangamfura Emmanuel <musangamfure@gmail.com>
2024-06-05 01:32:41 +0200

    fix the wishlist bug

--- app/javascript/controllers/wishlist_controller.js
+++ app/javascript/controllers/wishlist_controller.js
@@ -2,12 +2,23 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   updateWishlistStatus() {
-    if (this.element.classList.contains("fill-primary")) {
-      this.element.classList.remove("fill-primary");
-      this.element.classList.add("fill-none");
-    } else {
+    // get this status only if user logged-in else redirect to login page
+
+    const isUserLoggedIn = this.element.dataset.userLoggedIn;
+
+    if (isUserLoggedIn === "false") {
+      doccument.querySelector(".js-login").click();
+      return;
+    }
+
+    if (this.element.dataset.status === "false") {
       this.element.classList.remove("fill-none");
       this.element.classList.add("fill-primary");
+      this.element.dataset.status = "true";
+    } else {
+      this.element.classList.remove("fill-primary");
+      this.element.classList.add("fill-none");
+      this.element.dataset.status = "false";
     }
   }
 }
